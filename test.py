9be17448f176e7b11b90b5c4a1d0aacbc37dcc16
Adam-smh <africa.adam1@gmail.com>
2021-06-08 13:56:30 +0200

    Update

--- test.py
+++ test.py
@@ -7,3 +7,7 @@ class TestCal(unittest.TestCase):
     def test_add(self):
         mini_instance = MiniCalculator()
         self.assertEqual(mini_instance.add(2,2), 4, "Adding 2 t0 2 should = 4")
+
+    def test_subtract(self):
+        mini_instances = MiniCalculator()
+        self.assertEqual(mini_instances.subtract(2,2), 0, "it should be 0")
